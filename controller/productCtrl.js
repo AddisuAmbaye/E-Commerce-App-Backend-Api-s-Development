61d40ac7f7a71985f176ad3741046ad20e8c8ff1
addis <addeson3@gmail.com>
2023-10-25 22:25:17 -0700

    get products routes and ctrl

--- controller/productCtrl.js
+++ controller/productCtrl.js
@@ -1,15 +1,23 @@
-const Product = require('../models/productModel');
+const product = require('../models/productModel');
+const asyncHandler = require('express-async-handler');
 
-const createProduct = asyncHndler(async (req, res) => {
+// create product
+const createProduct = asyncHandler(async (req, res) => {
     const title = req.body.title;
-    const productFound = await Product.findOne({ title });
+    const productFound = await product.findOne({ title });
     if(!productFound){
-        const newProduct = new product.create(req.body);
+        const newProduct = await product.create(req.body);
         res.json(newProduct);
     }
     else{
         throw new Error("Product already exists.");
     }
+});
+
+// get all products
+const getProducts = asyncHandler(async (req, res) => {
+    const products = await product.find();
+    res.json(products);
 })
 
-module.exports = createProduct;
+module.exports = {createProduct, getProducts};
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
-const productRouter = express.router();
-const createProduct = require('../controller/productCtrl');
+const productRouter = express.Router();
+const {createProduct, getProducts} = require('../controller/productCtrl');
 
 productRouter.post("/post-product", createProduct);
-
+productRouter.get("/get-product", getProducts);
 module.exports = productRouter;
